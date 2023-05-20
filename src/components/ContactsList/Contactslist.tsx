ac0af1128dfb867e243e48516cd3034238cd4389
MelnykovSV <svmelnykov@gmail.com>
2023-05-20 19:47:01 +0300

    1

--- src/components/ContactsList/Contactslist.tsx
+++ src/components/ContactsList/Contactslist.tsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Container } from './Contactslist.styled';
-import { BiSearchAlt2 } from 'react-icons/bi';
+// import { BiSearchAlt2 } from 'react-icons/bi';
 import { StyledSearchIcon } from './Contactslist.styled';
 const shortid = require('shortid');
 
